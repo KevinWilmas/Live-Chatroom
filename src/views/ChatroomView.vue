445910e7c31663d1c5b092181650e97e5bb8c464
kevinwilmas <kevinwilmas@outlook.com>
2023-05-15 19:21:53 -0500

    Chatroomview logout function added (not finished)

--- src/views/ChatroomView.vue
+++ src/views/ChatroomView.vue
@@ -6,8 +6,20 @@
 
 <script setup>
 import NavbarComponent from '../components/NavbarComponent.vue'
-
+import getUser from '../composables/getUser.js'
+import { watch } from 'vue'
+import { useRouter } from 'vue-router'
 // import { ref } from 'vue'
+
+const router = useRouter()
+const { user } = getUser()
+
+// if the user value is ever null, redirect to welcome screen
+watch(user, () => {
+  if (!user.value) {
+    router.push({ name: 'WelcomeView' })
+  }
+})
 </script>
 
 <style scoped></style>
